0e207684d8777230bb3753e6d91c0727edd1ede9
YaelBusso <y0583239076@gmail.com>
2020-09-20 23:27:52 +0300

    Updates

---
+++ precache-manifest.217468b61ca603d1924f2318db9e0afa.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6dad8d45d231ad1a414573e6c1f3ca26",
+    "url": "/cody/index.html"
+  },
+  {
+    "revision": "5f2c2d367cbe0de98274",
+    "url": "/cody/static/css/2.c46397a6.chunk.css"
+  },
+  {
+    "revision": "9a00b4e1c9d1ac019ad0",
+    "url": "/cody/static/css/main.109fdfcc.chunk.css"
+  },
+  {
+    "revision": "5f2c2d367cbe0de98274",
+    "url": "/cody/static/js/2.922c0f9f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/cody/static/js/2.922c0f9f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9a00b4e1c9d1ac019ad0",
+    "url": "/cody/static/js/main.98d7eafc.chunk.js"
+  },
+  {
+    "revision": "9d4d355359a86f907ea2",
+    "url": "/cody/static/js/runtime-main.2a51223f.js"
+  }
+]);
